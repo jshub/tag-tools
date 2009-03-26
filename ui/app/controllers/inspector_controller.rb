71422df512816d97f505590f7dc6e43b32f798df
liamc <liamc@48407109-bd10-4b5e-b1b3-10390c1f52ac>
2009-03-26 17:50:57 +0000

    See #400 - rails controller for Inspector development
    git-svn-id: https://intra.causata.com/svn/javascript/tag-tools/trunk@2622 48407109-bd10-4b5e-b1b3-10390c1f52ac

---
+++ ui/app/controllers/inspector_controller.rb
@@ -0,0 +1,6 @@
+class InspectorController < ApplicationController
+
+  def index
+  end
+
+end
---
+++ ui/test/functional/inspector_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class InspectorControllerTest < ActionController::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
