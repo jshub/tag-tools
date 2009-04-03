a9d43298deb6782264283b65085b452f8081a66e
liamc <liamc@48407109-bd10-4b5e-b1b3-10390c1f52ac>
2009-04-03 10:37:18 +0000

    Capistrano config for the demo-store
    git-svn-id: https://intra.causata.com/svn/javascript/tag-tools/trunk@2841 48407109-bd10-4b5e-b1b3-10390c1f52ac

---
+++ demo-sites/retail/config/deploy.rb
@@ -0,0 +1,65 @@
+set :application, "retail"
+# The gateway server is accessed before anything else and all ssh commands sent via it
+set :gateway,     "www.etl2dot0.com"
+set :domain,      "gromit"
+set :rails_env,   "gromit"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to,   "/var/capistrano/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+set :scm,         "subversion"
+
+#If you log into your server with a different user name than you are logged 
+#into your local machine with, youÕll need to tell Capistrano about that user 
+#name.
+set :user, "dev"
+
+#If you access your source repository with a different user name than you are 
+#logged into your local machine with, Capistrano needs to know. Note that not 
+#all SCMÕs support the scm_username variable; you might need to embed the 
+#scm_username into the repository, 
+#e.g. Òsvn+ssh://#{scm_username}@foo.bar.com/path/to/repoÓ.
+set :scm_username, "capistrano"
+# Liam: needed due to Basic Auth protecting the SVN server
+set :scm_password, "tant0ine"
+
+# construct the path to the repository
+set :repository,  "http://#{domain}/svn/javascript/tag-tools/trunk/demo-sites/retail/"
+
+# all services are on the same server for now
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+
+#By default, Capistrano will try to use sudo to do certain operations (setting 
+#up your servers, restarting your application, etc.). If you are on a shared 
+#host, sudo might be unavailable to you, or maybe you just want to avoid using sudo.
+set :use_sudo,    false
+
+# Liam: overide the default task as we are using Passenger
+# ref: http://www.modrails.com/documentation/Users%20guide.html#capistrano
+namespace :deploy do
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+# additional custom tasks viewable with 'cap -T'
+namespace :custom do
+  desc 'Symlink the public directory into the web root. This is for use by Passenger via RailsBaseURI
+        ref: http://www.modrails.com/documentation/Users%20guide.html#deploying_rails_to_sub_uri'
+  task :symlink do
+    run "ln -nfs #{current_path}/public /var/www/html/#{application}"
+  end
+
+end
+# use our custom tasks at the appropriate time
+# e.g. before :deploy, :my_custom_task
+#      after  "deploy:symlink", :do_this, :and_do_that
+after "deploy:update",   "deploy:migrate"
+after "deploy:symlink",   "custom:symlink"
