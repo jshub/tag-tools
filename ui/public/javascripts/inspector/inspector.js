43b9ed3c97a55cb92edfa3da5ffd444025ea6ab7
steveh <steveh@48407109-bd10-4b5e-b1b3-10390c1f52ac>
2009-04-07 15:39:50 +0000

    trac 437,438,439,440 position inspector according to state, change width likewise
    git-svn-id: https://intra.causata.com/svn/javascript/tag-tools/trunk@2922 48407109-bd10-4b5e-b1b3-10390c1f52ac

--- ui/public/javascripts/inspector/inspector.js
+++ ui/public/javascripts/inspector/inspector.js
@@ -2,208 +2,6 @@
 
 
 
-function setup_inspector(inspector_id){
-	
-	var DOM = YAHOO.util.Dom,
-	    the_inspector,
-		// Used by height calculation - the height of the fixed parts of the inspector, ie everything except the event list accordion
-		_static_height;
-  
-  // init the panel
-  the_inspector = new YAHOO.widget.Panel(inspector_id, {
-        width: "225px",
-        draggable: true, 
-        close: true,
-        autofillheight: "body",
-        constraintoviewport: true
-  });
-  the_inspector.render();
-
-   var inspector_element = document.getElementById(inspector_id);
-   var ul = inspector_element.getElementsByTagName("ul");
-   if (ul.length==0){
-   		return;
-   }	
-   
-  // init the accordion inside the panel body
-  var eventList = new YAHOO.widget.AccordionView(ul[0], {
-        width: '100%', 
-        collapsible: true,
-        animate: false
-      });
-
-  // add contextual tooltips for event specific help using the title attribute
-  // TODO make this appear above the panel
-  var eventListHelp = new YAHOO.widget.Tooltip("eventListHelp", { 
-			  context: DOM.getElementsByClassName('help-text', 'div'),
-        	  constraintoviewport: true,
-			  width: "300px",
-			  zindex: 1000
-	});
-  
-  
-  // Make the panel resizable and handle events and repainting ref: http://developer.yahoo.com/yui/examples/container/panel-resize.html
-  // TODO account for open/closed accordion in recalculating the body height
-  var the_inspectorResize = new YAHOO.util.Resize('inspector-maximised', {
-    handles: ['br'],
-    autoRatio: false,
-    minWidth: 225,
-    minHeight: 290,
-    status: false
-  });
-  
-  
-  // initialize some fields needed by the height calculations
-  var inspector_height = the_inspector.innerElement.clientHeight;
-  _static_height = inspector_height - min_size_for_evtlist();
-
-  
-  the_inspectorResize.on("startResize", function(args) {
-  	
-	// set our minimum height
-	var min_height = minimum_inspector_height();
-    the_inspectorResize.set("minHeight", min_height);
-	
-    if (this.cfg.getProperty("constraintoviewport")) {
-        var clientRegion = DOM.getClientRegion();
-        var elRegion = DOM.getRegion(this.element);
-        the_inspectorResize.set("maxWidth", clientRegion.right - elRegion.left - YAHOO.widget.Overlay.VIEWPORT_OFFSET);
-        the_inspectorResize.set("maxHeight", clientRegion.bottom - elRegion.top - YAHOO.widget.Overlay.VIEWPORT_OFFSET);
-      } else {
-        the_inspectorResize.set("maxWidth", null);
-        the_inspectorResize.set("maxHeight", null);
-    }
-  }, the_inspector, true);
-  
-  
-  the_inspectorResize.on("resize", function(args) {
-    var panelHeight = args.height;
-    this.cfg.setProperty("height", panelHeight + "px");
-	
-	manage_height();
-	
-  }, the_inspector, true);
-  
-  
-  // lets try setting the initial height 
-  eventList.subscribe("afterPanelOpen",function(message){
-		manage_height(message.panel);
-  });
-  
-  function manage_height(selected_item){
-  	
-	if (!selected_item){
-		var panels = eventList.getPanels();
-		for (var i=0;i<panels.length;i++){
-			if (DOM.hasClass(panels[i].firstChild,eventList.CLASSES.ACTIVE)){
-				selected_item = panels[i];
-				break;
-			}
-		}
-	}
-	
-	if (!selected_item){
-		return;
-	}
-	
-	var chrome_height = 135;
-	var preferred_panel_height = 150;
-	var default_inspector_height = 450;
-	var min_panel_size = 100;
-
-	//TODO make this a bit more robust...
-	var content = selected_item.childNodes[1];
-
-	var auto_height = the_inspector.innerElement.style.height == "";
-	var inspector_height = the_inspector.innerElement.clientHeight;
-	
-	if (auto_height){
-		
-		var max_inspector_size = min_size_for_evtlist() + chrome_height + preferred_panel_height;
-		var preferred_height = Math.max(default_inspector_height,max_inspector_size);
-		
-		if (inspector_height > preferred_height){
-			var diff =  inspector_height - preferred_height;
-			var current_height = content.offsetHeight;
-			var new_height = current_height - diff;
-			content.style.height =new_height + "px";
-		}
-	}
-	else {
-		
-		var available_height = inspector_height - chrome_height - min_size_for_evtlist();
-		var content_height = content.offsetHeight;
-		var actual_content = content.getElementsByTagName("div");
-		if (actual_content.length){
-			var full_content_height = actual_content[0].offsetHeight;
-			if (content_height > available_height){
-				if (available_height < min_panel_size){
-					// we have to grow the inspector - this won't normally happen unless we really add a lot of categories
-					var new_inspector_height = inspector_height + (min_panel_size - available_height + 10);
-	    			the_inspector.cfg.setProperty("height", new_inspector_height + "px");
-				}
-				else {
-					content.style.height = available_height + "px";
-				}
-			} 
-			else if (full_content_height > content_height){
-				content.style.height = (available_height) + "px";
-			}
-		}
-	}		
-	
-  }
-  
-  function minimum_inspector_height(){
-  	
-		var preferred_panel_height = 150;
-		var event_list =   min_size_for_evtlist();
-		return _static_height + preferred_panel_height + event_list;
-			
-  }
-  
-  var _item_height;
-  
-  function min_size_for_evtlist(){
-
-	var items = eventList.getPanels();
- 	var count = items.length;
-	//TODO read item height from a collapsed item
-	return count * (_item_height || get_item_height());
-	
-  }
-
-  function get_item_height(){
-	var items = eventList.getPanels();
- 	var count = items.length;
-	var height = 0;
-	for (var i=0;i<count;i++){
-		if (DOM.hasClass(items[i].firstChild,eventList.CLASSES.ACTIVE)){
-			continue
-		}
-		else {
-			return _item_height = items[i].offsetHeight;
-		}
-	}
-	return 0;  	
-  } 	  
-
-	
-  // make this visible to outside page for testing...	
-  window.eventList = eventList;
-  window.manage_height = manage_height;
-
-	
-	
-	
-};
-
-
-
-
-
-
-
 
 var jshub = {};
 
@@ -313,13 +111,17 @@ var jshub = {};
 				div = document.body.appendChild(document.createElement("div"));
 				div.id = "jshub-inspector-container";
 				div.className = "yui-cssreset yui-cssfonts yui-cssgrids yui-cssbase jshub inspector yui-skin-sam example-ui";
+				div.style.position = "absolute";
+				div.style.height = "0px";
+				div.style.width = "0px";
+				
 			}
 			else {
 				div.innerHTML = "";
 			}			
 			
 			 var panel = new YAHOO.widget.Panel("jshub_inspector", {
-			        width: "225px",
+			        width: "95px",
 			        draggable: true, 
 			        close: false,
 			        autofillheight: "body",
@@ -406,6 +208,8 @@ var jshub = {};
 			YAHOO.util.Event.addListener(container_close, "click", function(e){e.preventDefault();self.set_display_state("state1");});
 		
 		this.rendered = true;
+		
+		return this;
 		
 	}
     /**
@@ -660,18 +464,28 @@ var jshub = {};
 
 	};
 
+	Inspector.prototype.get_display_state = function(state){
+		return this._display_state;
+	};
+
 	Inspector.prototype.set_display_state = function(state){
-	  // update the maximised example
-	  var inspectorBody = document.getElementById('jshub_inspector');
-	  // clear the existing states
-	  DOM.removeClass(inspectorBody, 'state1');
-	  DOM.removeClass(inspectorBody, 'state2');
-	  DOM.removeClass(inspectorBody, 'state3');
+	 	// update the maximised example
+	  	if (state == this._display_state){
+	  		 return;
+	  	}
 	  
-	  // add the state class to the body of the inspector for contextual CSS switching
-	  DOM.addClass(inspectorBody, state);
+		var inspectorBody = document.getElementById('jshub_inspector');
+		// clear the existing states
+		DOM.removeClass(inspectorBody, 'state1');
+		DOM.removeClass(inspectorBody, 'state2');
+		DOM.removeClass(inspectorBody, 'state3');
+	  
+	  	// add the state class to the body of the inspector for contextual CSS switching
+	  	DOM.addClass(inspectorBody, state);
 
-
+		var previous_state = this._display_state;
+		this._display_state = state;
+		
 		var inner_element = this.yuipanel.innerElement;
 		var auto_height = inner_element.style.height == "";
 		
@@ -695,6 +509,21 @@ var jshub = {};
 				this.resizer.resize(null,element.offsetHeight,element.offsetWidth,0,0,true,true);
 			}
 		}	
+		
+		if (state == "state1"){
+			this.yuipanel.cfg.setProperty("width", "95px");
+		}
+		else if (previous_state == "state1"){
+			this.yuipanel.cfg.setProperty("width", "225px");
+		}
+		
+		// Can't position until both dimensions have been set
+		if (state == "state1"){
+			this.position("br");
+		}
+		else if (previous_state == "state1"){
+			this.position("tr");
+		}
 		
 	  if (state == "state3" && typeof this._category_item_height == "undefined"){
  		  var inspector_div = document.getElementById("jshub_inspector");
@@ -879,6 +708,42 @@ var jshub = {};
 	  }
 
     };
+
+	Inspector.prototype.show = function(){
+		var div = document.getElementById("jshub-inspector-container");
+		div.style.display = "";
+	};
+	
+	Inspector.prototype.hide = function(){
+		var div = document.getElementById("jshub-inspector-container");
+		div.style.display = "none";
+	};
+	
+
+	
+	Inspector.prototype.position = function(position){
+		
+		var dimensions = _get_available_space();
+		//alert("width: " + dimensions.width + "\nheight:" + dimensions.height);
+		
+		var element = this.yuipanel.innerElement;
+		
+		var width = element.offsetWidth;
+		var height = element.offsetHeight;
+
+		if (position == "br"){
+			var left = dimensions.width - width - 20;
+			var top = dimensions.height - height - 10;
+		}
+		else if (position == "tr"){
+			var left = dimensions.width - width - 20;
+			var top = 0;
+		}
+
+		this.yuipanel.cfg.setProperty("xy",[left,top]);		
+		
+	};
+	
 	
 	function _create_header(){
 		return '<span class="title">Activity Inspector</span><a class="container-minimise" href="#">Minimise</a><a class="container-close" href="#">Close</a>'; 
@@ -1089,6 +954,37 @@ var jshub = {};
 	jshub.Inspector = new Inspector;
 	jshub.Inspector.render("state1");
 	jshub.Inspector.init();
+
+
+	// borrowed from leon...
+	function _get_available_space(){
+		
+		if(typeof(window.innerHeight) == 'number') {
+	    	var maxW = window.innerWidth;
+	    	var maxH = window.innerHeight;
+	  	}
+	  	else if(document.documentElement && document.documentElement.clientHeight) {
+	    	var maxW = parseInt(document.documentElement.clientWidth);
+	    	var maxH = parseInt(document.documentElement.clientHeight);
+	  	} 
+	  	else if( document.body && document.body.clientHeight){
+	    	var maxW =  parseInt(document.body.clientWidth);
+	    	var maxH =  parseInt(document.body.clientHeight);
+	    }	
+	    else { // we're screwed
+	    	var maxW = 800;
+	    	var maxH = 600;
+	    }	
+
+//		var $dimensions = {top:0,left:0,width:maxW,height:maxH,scrollTop:document.body.scrollTop};
+		var dimensions = {top:0,left:0,width:maxW,height:maxH};
+	
+		if (typeof dimension != "undefined"){
+			return dimensions[dimension];
+		}
+		
+		return dimensions;
+	}
 
 	
 	
