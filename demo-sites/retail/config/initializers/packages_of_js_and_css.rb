996dbd2dc68323ec1bd77559fd28b6b71ead0954
liamc <liamc@48407109-bd10-4b5e-b1b3-10390c1f52ac>
2009-04-20 15:52:36 +0000

    Clarifying dependencies between the store, jsHub and the Inspector. Removing load order dependencies. Don't know how this working in the demo without JSON being present?
    git-svn-id: https://intra.causata.com/svn/javascript/tag-tools/trunk@3156 48407109-bd10-4b5e-b1b3-10390c1f52ac

--- demo-sites/retail/config/initializers/packages_of_js_and_css.rb
+++ demo-sites/retail/config/initializers/packages_of_js_and_css.rb
@@ -1,9 +1,13 @@
-# Inspector files
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion :yui => ["inspector/yui2/utilities/utilities", "inspector/yui2/dragdrop/dragdrop", "inspector/yui2/container/container", "inspector/yui2/resize/resize", "inspector/accordionview/accordionview"]
-ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :yui => ["inspector/yui3/reset-context", "inspector/yui3/fonts-context", "inspector/yui3/grids-context", "inspector/yui3/base-context", "inspector/yui2/container/assets/skins/sam/container", "inspector/yui2/resize/assets/skins/sam/resize", "inspector/accordionview/accordionview"]
+# Store + dependencies
+ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :store_yui => ["inspector/yui3/reset-context", "inspector/yui3/fonts-context", "inspector/yui3/grids-context", "inspector/yui3/base-context"]
+ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :store => ["store", "hproduct", "microformats"]
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :store => ["jquery/jquery-1.3.2.min", "store"]  
 
+# jsHUb core + dependencies
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jshub => ["jquery/jquery-1.3.2.min", "json/json2"]
   
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jshub => ["jquery/jquery-v1.2.6", "json/json2"]
-  
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion :inspector => ["inspector/sha1", "inspector/inspector"]
+# jsHub Inspector + dependencies
+ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :inspector_yui => ["inspector/yui3/reset-context", "inspector/yui3/fonts-context", "inspector/yui3/grids-context", "inspector/yui3/base-context", "inspector/yui2/container/assets/skins/sam/container", "inspector/yui2/resize/assets/skins/sam/resize", "inspector/accordionview/accordionview"]
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :inspector_yui => ["inspector/yui2/utilities/utilities", "inspector/yui2/dragdrop/dragdrop", "inspector/yui2/container/container", "inspector/yui2/resize/resize", "inspector/accordionview/accordionview"]
 ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :inspector => ["inspector/inspector"]
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :inspector => ["json/json2", "inspector/sha1", "inspector/inspector"]
