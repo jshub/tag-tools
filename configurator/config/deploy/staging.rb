3b99da1835fff205b7b8561cf66be77a32e69dc3
fianno <fianno@48407109-bd10-4b5e-b1b3-10390c1f52ac>
2009-06-17 12:59:12 +0000

    See #583, applied same capistrano changes to the configurator app
    git-svn-id: https://intra.causata.com/svn/javascript/tag-tools/trunk@4658 48407109-bd10-4b5e-b1b3-10390c1f52ac

---
+++ configurator/config/deploy/staging.rb
@@ -0,0 +1,26 @@
+# STAGING-specific deployment configuration
+# please put general deployment config in config/deploy.rb
+
+puts "Deploying to STAGING"
+
+# The gateway server is accessed before anything else and all ssh commands sent via it
+set :gateway,     "intra.causata.com"
+set :scm_domain,  "gromit"
+set :domain,      "gromit"
+set :rails_env,   "gromit"
+
+# construct the path to the repository
+set :repository,   "http://#{scm_domain}/svn/javascript/tag-tools/trunk/configurator/"
+
+#If you log into your server with a different user name than you are logged 
+#into your local machine with, youll need to tell Capistrano about that user 
+#name.
+set :user, "dev"
+
+# webserver root symlink path for passenger
+set :webroot, "/var/www/html/#{application}"
+
+# all services are on the same server for now
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
