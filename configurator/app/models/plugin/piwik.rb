0814bf8587b6500607736d818feef2e1d1569e38
fianno <fianno@48407109-bd10-4b5e-b1b3-10390c1f52ac>
2009-06-17 15:32:43 +0000

    Just a bit of formatting
    git-svn-id: https://intra.causata.com/svn/javascript/tag-tools/trunk@4666 48407109-bd10-4b5e-b1b3-10390c1f52ac

--- configurator/app/models/plugin/piwik.rb
+++ configurator/app/models/plugin/piwik.rb
@@ -1,15 +1,14 @@
-  
-  class Plugin::Piwik < Plugin
-    def self.instance
-      @instance ||= find_or_create_by_type self.name
-    end
-    
-    def js_files
-      ['data-output/piwik-plugin.js']
-    end
-    
-    def plugin_type
-      :data_transport
-    end
+class Plugin::Piwik < Plugin
+  def self.instance
+    @instance ||= find_or_create_by_type self.name
   end
   
+  def js_files
+    ['data-output/piwik-plugin.js']
+  end
+  
+  def plugin_type
+    :data_transport
+  end
+end
+
