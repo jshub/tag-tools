c4623b6ef72f290abcd5f873bc6df6ee2feec5a5
fianno <fianno@48407109-bd10-4b5e-b1b3-10390c1f52ac>
2009-06-25 10:39:48 +0000

    Update SVN domain
    git-svn-id: https://intra.causata.com/svn/javascript/tag-tools/trunk@4824 48407109-bd10-4b5e-b1b3-10390c1f52ac

--- configurator/config/deploy/staging.rb
+++ configurator/config/deploy/staging.rb
@@ -5,12 +5,12 @@ puts "Deploying to STAGING"
 
 # The gateway server is accessed before anything else and all ssh commands sent via it
 set :gateway,     "intra.causata.com"
-set :scm_domain,  "gromit"
+set :scm_domain,  "intra.causata.com"
 set :domain,      "gromit"
 set :rails_env,   "gromit"
 
 # construct the path to the repository
-set :repository,   "http://#{scm_domain}/svn/javascript/tag-tools/trunk/configurator/"
+set :repository,   "https://#{scm_domain}/svn/javascript/tag-tools/trunk/configurator/"
 
 #If you log into your server with a different user name than you are logged 
 #into your local machine with, youll need to tell Capistrano about that user 
