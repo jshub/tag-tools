74621d7596c7f93eb5db2505b34b86c90a709f36
liamc <liamc@48407109-bd10-4b5e-b1b3-10390c1f52ac>
2009-06-26 10:41:14 +0000

    Enable JS Unit testing in UI app, e.g. for Inspector (copied from jshub-core.git)
    git-svn-id: https://intra.causata.com/svn/javascript/tag-tools/trunk@4875 48407109-bd10-4b5e-b1b3-10390c1f52ac

--- ui/config/initializers/packages_of_js_and_css.rb
+++ ui/config/initializers/packages_of_js_and_css.rb
@@ -1,3 +1,26 @@
+# Shared by all application views
+ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :jshub => [
+  "jshub.css"] 
+
+# YUI is used by unit tests
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :yui3 => [
+  "yui-3.0.0pr2/build/yui/yui.js",
+  "yui-3.0.0pr2/build/oop/oop.js", 
+  "yui-3.0.0pr2/build/event/event.js"]
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :yuitest => [
+  "yui-3.0.0pr2/build/dump/dump.js", 
+  "yui-3.0.0pr2/build/substitute/substitute.js", 
+  "yui-3.0.0pr2/build/dom/dom.js", 
+  "yui-3.0.0pr2/build/node/node.js", 
+  "yui-3.0.0pr2/build/json/json.js", 
+  "yui-3.0.0pr2/build/yuitest/yuitest.js"]
+  
+# Unit test assets
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :javascript_unit_test => [
+  "javascript_unit_test.js"]
+ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :javascript_unit_test => [
+  "javascript_unit_test.css"]
+
 # jsHUb core + dependencies
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jshub => ["jquery/jquery-1.3.2.min", "json/json2"]
   
@@ -6,3 +29,4 @@ ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :inspector_yui
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion :inspector_yui => ["inspector/yui2/utilities/utilities", "inspector/yui2/dragdrop/dragdrop", "inspector/yui2/container/container", "inspector/yui2/resize/resize", "inspector/accordionview/accordionview"]
 ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :inspector => ["inspector/inspector"]
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion :inspector => ["json/json2", "inspector/sha1", "inspector/inspector"]
+
