2738e36cd40adb5030f28ee64213016973853e42
fianno <fianno@48407109-bd10-4b5e-b1b3-10390c1f52ac>
2009-06-26 10:14:03 +0000

    Add webrat gem
    git-svn-id: https://intra.causata.com/svn/javascript/tag-tools/trunk@4870 48407109-bd10-4b5e-b1b3-10390c1f52ac

--- configurator/config/environment.rb
+++ configurator/config/environment.rb
@@ -42,6 +42,8 @@ Rails::Initializer.run do |config|
   config.gem "haml", :version => '2.0.9'
   # user authentication
   config.gem "authlogic"
+  # integration testing
+  config.gem "webrat"
   
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
