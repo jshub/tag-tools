04a72e805f50b37c9e6046dd0b9aadd59592a16d
liamc <liamc@48407109-bd10-4b5e-b1b3-10390c1f52ac>
2009-06-28 21:07:55 +0000

    Inspector refactoring - commited broken generated jsHub file
    git-svn-id: https://intra.causata.com/svn/javascript/tag-tools/trunk@4929 48407109-bd10-4b5e-b1b3-10390c1f52ac

--- ui/public/javascripts/jshub/jshub-gromit-r7-debug.js
+++ ui/public/javascripts/jshub/jshub-gromit-r7-debug.js
@@ -13,7 +13,7 @@
     Homepage: "http://www.jshub.org/",
     Version: "0.1beta",
     GeneratedBy: "http://localhost:10081/configurator/tag_configurations",
-    Configuration: "Test r4927 (revision 1, debug)"
+    Configuration: "Test r4927 - no output plugins (revision 1, debug)"
   };
   this.jsHub = this.jsHub || {};
   for (var field in tag_data) {
@@ -1705,89 +1705,6 @@
 /*
  * Data Transport Plug-ins
  *//*--------------------------------------------------------------------------*/
-
-/** 
- * A sample plugin to capture jsHub events and send them to a server via a 
- * single pixel gif image.
- * 
- * You can use this as a starting point to customize the data to generate a
- * URL in the format expected by your server.
- *
- * @module data-transport
- * @class sample-get-plugin
- */
-/*--------------------------------------------------------------------------*/
-
-"use strict";
-
-(function() {
-
-  /**
-   * Metadata about this plug-in for use by UI tools and the Hub
-   */
-  var metadata = {
-  	id: 'sample-get-plugin',
-    name: 'Sample HTTP GET transport plugin',
-    version: 0.1,
-    author: "Fiann O'Hagan",
-    email: 'fiann.ohagan@jshub.org',
-    vendor: 'jsHub'
-  },  
-  
-  /**
-   * The events that will be captured and sent to the server
-   */
-  boundEvents = ['page-view', 'authentication', 'checkout'],  
-  
-  /**
-   * Event driven anonymous function bound to 'page-view'
-   * @method send
-   * @param event {Object} the event to serialize and send to the server
-   * @property metadata
-   */
-  send = function(event) {
-  
-    jsHub.logger.group("Sample get transport: sending '%s' event", event.type);
-    
-    /**
-     * Account ID for the client
-     * Note that the field <code>account_id</code> in the string is replaced
-     * when the tag is generated.
-     */
-    var account = "1234";
-    
-    /**
-     * URL to dispatch to the server
-     * Note that the field <code>server_url</code> in the string is replaced
-     * when the tag is generated.
-     */
-    var url = "http://localhost//account/" + account;
-    
-	/**
-	 * Each field in this object is serialized as a name=value pair in the query
-	 * string of the URL that is created for the image request.
-	 * You can put any data in this object. If the value of a field is an array,
-	 * then it will be used to generate multiple name=value pairs in the resulting
-	 * query string.
-	 */
-    var data = {
-      sender: metadata.name + " v" + metadata.version,
-      pagename: event.data.name
-    };
-    
-    // dispatch via API function
-    jsHub.dispatchViaImage(url, data);
-    jsHub.logger.groupEnd();
-  };
-  
-  /*
-   * Bind the plugin to the Hub so as to run when events we are interested in occur
-   */
-  jsHub.bind("page-view", metadata.id, send);
-  
-  // lifecycle notification
-  jsHub.trigger("plugin-initialization-complete", metadata);
-})();
 
 
 
